622d776d70bef9d6177f7ab6d0e2fa42128d215a
ALEXANDROS RODRIGUES KOTZIAS <alexandros_kotzias@estudante.sesisenai.org.br>
2024-04-11 21:59:37 -0300

    atividade1

--- javaFor/src/Main.java
+++ javaFor/src/Main.java
@@ -1,36 +1,43 @@
 import javax.swing.*;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+////TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+//// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-//       String s1,s2;
-//       s1 = JOptionPane.showInputDialog("S1: ");
-//       s2 = JOptionPane.showInputDialog("S2: ");
-//       //s2 = s1;
-//
-////     if(s1 == s20){ isso da muito ruim
-//       if(s1.equals(s2)){
-//           System.out.println("iguais");
-//       }else{
-//           System.out.println("diferentes");
-//       }2
-        int maior = 0;
-        int valido = 100;
-        int pode;
+////       String s1,s2;
+////       s1 = JOptionPane.showInputDialog("S1: ");
+////       s2 = JOptionPane.showInputDialog("S2: ");
+////       //s2 = s1;
+////
+//////     if(s1 == s20){ isso da muito ruim
+////       if(s1.equals(s2)){
+////           System.out.println("iguais");
+////       }else{
+////           System.out.println("diferentes");
+////       }2
+////        int maior = 0;
+////        int valido = 100;
+////        int pode;
+////
+////        for (int i = 0; i < 10; i++) {
+////            int numDigitado;
+////            numDigitado = Integer.parseInt(JOptionPane.showInputDialog("Digite um número de 0 a 100: "));
+////             if (numDigitado < valido){
+////                pode = numDigitado;
+////                if(pode > maior){
+////                    maior = pode;
+////                }
+////            }else {
+////                System.out.println("Número não valido.");
+////            }
+////        }
+////        System.out.println(maior);
 
-        for (int i = 0; i < 10; i++) {
-            int numDigitado;
-            numDigitado = Integer.parseInt(JOptionPane.showInputDialog("Digite um número de 0 a 100: "));
-            if (numDigitado < valido){
-                pode = numDigitado;
-                if(pode > maior){
-                    maior = pode;
-                }
-            }else {
-                System.out.println("Número não valido.");
-            }
+        int quantos = JOptionPane.showInputDialog("Quantos salarios voce ira digitar: ");
+        int salarios [] = new int[quantos];
+        for (int i = 0; i < quantos; i++) {
+            
         }
-        System.out.println(maior);
+
     }
 }
